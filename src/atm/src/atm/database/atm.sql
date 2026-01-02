fbdaa698ee9e5164699d085f91513edca0a481f0
Madhavi-IT <vm9279488@gmail.com>
2026-01-02 13:26:05 +0530

    Create atm.sql

---
+++ src/atm/src/atm/database/atm.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE atm_db;
+USE atm_db;
+
+CREATE TABLE users (
+    card_number VARCHAR(20) PRIMARY KEY,
+    pin INT,
+    balance DOUBLE
+);
+
+INSERT INTO users VALUES ('123456', 1234, 5000);
+INSERT INTO users VALUES ('654321', 4321, 10000);
